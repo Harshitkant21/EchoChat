8e35a37273f5af1cbcc6229bfdadfddd3caad35b
Harshit Kant <harshitkant12@gmail.com>
2024-06-20 01:08:18 +0530

    created user management backend

---
+++ server/models/userModel.js
@@ -0,0 +1,32 @@
+// this file contains the user form informations basically the info which is require to form a database of the project 
+
+import mongoose from "mongoose";
+
+const userModel = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePhoto: {
+      type: String,
+      default: "",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: true,
+    }
+  },
+  { timestamps: true }
+);
+export const User = mongoose.model("User", userModel);
