7fb918f0f825559cfaf367ec0e6117524f932c9b
Harshit Kant <harshitkant12@gmail.com>
2024-06-22 01:50:54 +0530

    added signup page

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"], // Ensure all relevant file types are included
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ]
+}
